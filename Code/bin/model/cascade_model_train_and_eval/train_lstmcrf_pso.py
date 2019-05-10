59b34db9dc22534a1fefe821fdb485b2af6d547a
weiminggao <1959839061@qq.com>
2019-05-10 14:08:49 +0800

    branch merged

--- Code/bin/model/cascade_model_train_and_eval/train_lstmcrf_pso.py
+++ Code/bin/model/cascade_model_train_and_eval/train_lstmcrf_pso.py
@@ -7,10 +7,73 @@ sys.path.append('../cascade_model')
 from load_data import process_data
 import pso_lstmcrf
 
-RESTORE = False
+RESTORE = True
 
 def evaluate(process_data):
-    pass
+    pred_correct_num = 0
+    pred_num = 0
+    real_num = 0
+    def stats(predict_output, label):
+        def convert_to_num_str(tags):
+            num_str = []
+            for i in range(len(tags)):
+                if tags[i] == 3:#表示B
+                    start_flag = str(i)
+                    j = i + 1
+                    while j < len(tags) and tags[j] != 0 and tags[j] != 3:
+                        j += 1
+                    start_flag += str(j)
+                    num_str.append(start_flag)
+            return num_str
+        
+        nonlocal pred_correct_num, pred_num, real_num
+        for i in range(len(predict_output)):
+            pred_num_str = convert_to_num_str(predict_output[i])
+            lab_num_str = convert_to_num_str(label[i])
+            pred_num += len(pred_num_str)
+            real_num += len(lab_num_str)
+            for ele in pred_num_str:
+                if ele in lab_num_str:
+                    pred_correct_num += 1
+    
+    word_placeholder = tf.placeholder(tf.int32, [None, process_data.max_len]) 
+    postag_placeholder = tf.placeholder(tf.int32, [None, process_data.max_len]) 
+    p_placeholder = tf.placeholder(tf.int32, [None])
+    s_placeholder = tf.placeholder(tf.int32, [None, 5])
+    sequence_lengths = tf.placeholder(tf.int32, [None])
+    out_placeholder = tf.placeholder(tf.int32, [None, process_data.max_len])
+    batch_length = tf.placeholder(tf.int32)
+    
+    pso_model = pso_lstmcrf.pso_lstmcrf_model(word_placeholder, postag_placeholder, p_placeholder, s_placeholder, \
+                                              np.shape(process_data.word_embedding), np.shape(process_data.postag_embedding), \
+                                              np.shape(process_data.p_embedding), process_data.word_embedding, process_data.postag_embedding, \
+                                              process_data.p_embedding, sequence_lengths, batch_length)
+    log_likelihood, transition_params = tf.contrib.crf.crf_log_likelihood(pso_model, out_placeholder, sequence_lengths)
+    decode_tags, best_score = tf.contrib.crf.crf_decode(pso_model, transition_params, sequence_lengths)
+    test_data_iter = process_data.generate_batch(batch_size, process_data.test_data, \
+                                                 features = ['word_embedding', 'postag', 'p', 's', 'sequence_lengths'], label_type = 'o')
+    saver = tf.train.Saver()	
+    
+    with tf.Session() as sess:
+        saver.restore(sess, tf.train.latest_checkpoint('./lstmcrf_pso_model'))
+#        saver.restore(sess, './lstmcrf_ps_model\lstmcrf_pso.ckpt0.0014773-72100')
+        decode_tags, best_score = tf.contrib.crf.crf_decode(pso_model, transition_params, sequence_lengths)
+        try:
+            data, label = test_data_iter.__next__()
+            while data:
+                _decode_tags = sess.run(decode_tags, feed_dict = {word_placeholder:data['word_embedding'], postag_placeholder:data['postag'], \
+                                                                  p_placeholder:data['p'], s_placeholder:data['s'],\
+                                                                  sequence_lengths:data['sequence_lengths'], batch_length:len(data['sequence_lengths'])})		
+                stats(_decode_tags, label)
+                data, label = test_data_iter.__next__()
+        except Exception as e:
+            print('预测完毕')
+            precision = pred_correct_num / pred_num
+            recall = pred_correct_num / real_num
+            f1 = (2 * precision * recall)/(precision + recall)
+            print('pred_correct_num:{}, pred_num:{}, real_num:{}'.format(pred_correct_num, pred_num, real_num))
+            print('precision:{},recall:{},f1:{}'.format(precision, recall, f1))
+    return f1, precision, recall
         
 def train(learning_rate, batch_size, epoch, process_data):
     word_placeholder = tf.placeholder(tf.int32, [None, process_data.max_len]) 
@@ -70,7 +133,8 @@ if __name__ == '__main__':
     p_path = '../../../data/all_50_schemas'
     process_data = process_data(train_data_path_list, test_data_path, pre_word_embedding_path, baike_word_embedding_path, postag_path, p_path)
     
-    batch_size = 128
-    learning_rate = 0.0001    #0.0000001收敛较慢
+    batch_size = 256
+    learning_rate = 0.001    #0.0000001收敛较慢
     epoch = 100
     train(learning_rate, batch_size, epoch, process_data)
+    evaluate(process_data)
