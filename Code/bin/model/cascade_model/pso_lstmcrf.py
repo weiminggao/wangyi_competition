474a0747673d4285f41cd534f07fd70fae28e902
weiminggao <1959839061@qq.com>
2019-05-15 11:03:21 +0800

    'pso_s_first_p_second'

--- Code/bin/model/cascade_model/pso_lstmcrf.py
+++ Code/bin/model/cascade_model/pso_lstmcrf.py
@@ -17,21 +17,8 @@ def pso_lstmcrf_model(word, postag, p, s, word_embedding_size, postag_embedding_
     with tf.name_scope('input_representation'):
         input_representation = tf.concat([word_embed, postag_embed], 2)
         input_representation = tf.cast(input_representation, tf.float32)
-        
-    #p_attention_diag
-    with tf.name_scope('p_attention'):	 
-        p_attention_m = tf.get_variable('p_attention_M', shape = [word_embedding_size[1] + postag_embedding_size[1], p_embedding_size[1]])
-        p_attention_m = tf.tile(tf.expand_dims(p_attention_m, 0), [batch_size, 1, 1])		
-        p_wm = tf.matmul(input_representation, p_attention_m)		
-        p_transpose = tf.transpose(tf.expand_dims(p_embed, 1), [0, 2, 1])
-        p_wmr = tf.matmul(p_wm, p_transpose)
-        p_bias = tf.get_variable('p_bias', shape = [1])
-        p_wmr = p_wmr + p_bias#加法是在最后一维上加
-        p_diag = tf.reshape(p_wmr, [batch_size, -1])
-        p_diag = tf.nn.softmax(p_diag)
-        p_diag = tf.reshape(p_diag, [batch_size, 198, -1])
-        
-    #s_attension_diag
+
+    #s_attention_diag
     with tf.name_scope('s_attention'):
 #        s_input_representation = tf.multiply(input_representation, p_diag)
         s_attention_m = tf.get_variable('s_attention_M', shape = [word_embedding_size[1] + postag_embedding_size[1], word_embedding_size[1]])
@@ -45,10 +32,25 @@ def pso_lstmcrf_model(word, postag, p, s, word_embedding_size, postag_embedding_
         s_diag = tf.nn.softmax(s_diag)
         s_diag = tf.reshape(s_diag, [batch_size, 198, 1])
     
+    #p_attention_diag
+    with tf.name_scope('p_attention'):
+        p_input_representation = tf.multiply(input_representation, s_diag)
+        p_attention_m = tf.get_variable('p_attention_M', shape = [word_embedding_size[1] + postag_embedding_size[1], p_embedding_size[1]])
+        p_attention_m = tf.tile(tf.expand_dims(p_attention_m, 0), [batch_size, 1, 1])		
+        p_wm = tf.matmul(p_input_representation, p_attention_m)		
+        p_transpose = tf.transpose(tf.expand_dims(p_embed, 1), [0, 2, 1])
+        p_wmr = tf.matmul(p_wm, p_transpose)
+        p_bias = tf.get_variable('p_bias', shape = [1])
+        p_wmr = p_wmr + p_bias#加法是在最后一维上加
+        p_diag = tf.reshape(p_wmr, [batch_size, -1])
+        p_diag = tf.nn.softmax(p_diag)
+        p_diag = tf.reshape(p_diag, [batch_size, 198, -1])
+        
     #bi_lstm
     with tf.name_scope('bi_lstm'):
-        ps_diag = tf.add(p_diag, s_diag)
-        lstm_input = tf.multiply(input_representation, ps_diag)
+#        ps_diag = tf.add(p_diag, s_diag)
+#        p_input_representation = tf.multiply(input_representation, s_diag)
+        lstm_input = tf.multiply(p_input_representation, p_diag)
         cell_fw = tf.contrib.rnn.LSTMCell(512)
         cell_bw = tf.contrib.rnn.LSTMCell(512)
         (output_fw, output_bw), _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, lstm_input, \
